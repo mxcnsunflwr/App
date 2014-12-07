c876a7eb43cab21601c1ca6016296ba1b5306603
Marcela <mt@mts-MacBook-Pro.local>
2014-12-06 16:31:42 -0800

    update Lotto Scanner with Camera Plugin

---
+++ _js/author.js
@@ -0,0 +1,12 @@
+// take photo on Camera
+function takePhoto() {
+	navigator.camera.getPicture(onSuccess, onFail,{quality: 100, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA, encodingType: Camera.EncodingType.JPEG})
+}
+function onSuccess(imageURI) {
+	var image = document.getElementbyID('ticketImage');
+	image.src = imageURI;
+	image.style.display = 'block';
+}
+function onFail(message) {
+	alert('Failed because: ' + message);
+}
