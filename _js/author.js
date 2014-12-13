79f9ecaea402e44c5b91cc87023486b262a96147
Marcela <mt@mts-MacBook-Pro.local>
2014-12-13 15:07:57 -0800

    updated

--- _js/author.js
+++ _js/author.js
@@ -1,23 +1,5 @@
-// take photo on Camera
-function takePhoto() {
-	navigator.camera.getPicture(onSuccess, onFail,{quality: 100, destinationType: Camera.DestinationType.FILE_URI, sourceType: Camera.PictureSourceType.CAMERA, encodingType: Camera.EncodingType.JPEG})
-}
-function onSuccess(imageURI) {
-	var image = document.getElementbyID('ticketImage');
-	image.src = imageURI;
-	image.style.display = 'block';
-}
-function onFail(message) {
-	alert('Failed because: ' + message);
-}
-
 // ALERT WINNER***************************
 
-if (x === 'match') {
-	alert('#box p');
-}
-else {
-	alert('Sorry');
 // ****************************************
 
 // SCANNER*********************************
@@ -37,9 +19,9 @@ document.addEventListener("deviceready", onDeviceReady, false);
 					"Result: " + result.text + "\n" +
 					"Format: " + result.format + "\n" +
 					"Cancelled: " + result.cancelled);
-				},
+			},
 			function (error) {
 				alert("Scanning failed: " + error);
 			}
-  );
-}
+  		);
+	}
