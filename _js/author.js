a2e7fd3e9cea941d2dca404b03ce723de6a95cb6
Marcela <mt@mts-MacBook-Pro.local>
2014-12-18 21:20:15 -0800

    week11 update

--- _js/author.js
+++ _js/author.js
@@ -1,3 +1,14 @@
+// WINNER
+	$(function () {
+	    setInterval(function () {
+		    $('#box p').fadeOut(150).delay(120).fadeIn(300).fadeOut(150).delay(120);
+	        $('#box p').fadeOut(300).fadeIn(120).fadeOut(120).delay(120);
+	        $('#box p').fadeOut(150).delay(120).fadeIn(300).fadeOut(150).delay(120);
+	        $('#box p').fadeOut(700).fadeIn(300).fadeOut(160).delay(120);
+	    }, 1);
+    });
+
+
 // Wait for PhoneGap to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
